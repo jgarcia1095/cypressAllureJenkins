5134ec4b5b68824d9e4df6a6bdf70d2545827145
JhoanGarcia <jhoan.garcia@avaldigitallabs.com>
2021-08-30 19:19:19 -0500

    cucumber configuration

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -23,7 +23,11 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 }
 
+const cucumber = require('cypress-cucumber-preprocessor').default
 
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
+}
 
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
