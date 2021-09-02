ede03733336cd1f246e572e69baf288704d5f406
JhoanGarcia <jhoan.garcia@avaldigitallabs.com>
2021-09-01 22:43:41 -0500

    plugin index cucumber

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,10 +22,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
-
+const cucumber = require('cypress-cucumber-preprocessor').default
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = (on, config) => {
+  on('file:preprocessor', cucumber())
   allureWriter(on, config);
   return config;
 };
